d3870ef4e93e802887b65db863e0b13e6bf32fbc
Sani Kamal <sanikamal223@gmail.com>
2018-12-15 02:12:03 +0530

    Update views.py
    key remove

--- weather/views.py
+++ weather/views.py
@@ -4,8 +4,7 @@ from .models import City
 from .forms import CityForm
 
 def index(request):
-        # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=631d05ff6393f7a5c5e6a68d4943d852'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
     # city='Delhi'
     if request.method == 'POST':
         form = CityForm(request.POST)
